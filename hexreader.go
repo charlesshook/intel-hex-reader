b5fc8a3ff2057bc87173c4d7102e5b98c3fbbaf4
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2020-04-12 20:21:33 -0500

    Added intel hex record types

--- hexreader.go
+++ hexreader.go
@@ -1,1 +1,21 @@
 package hexreader
+
+//Intel hex record type
+type RecType int
+
+/*
+	DATA - Data record type
+	EOF - End of file record type
+	EST - Extended segment address record type
+	SSA - Start segment address record type
+	ELA - Extended linear address record type
+	SLA - Start linear address
+*/
+const (
+	DATA	RecType = iota
+	EOF	
+	EST
+	SSA
+	ELA
+	SLA
+)
