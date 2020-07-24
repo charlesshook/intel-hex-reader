1c8e9489a3d71e08b138bcb6f55da808a343ac83
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2020-07-24 13:41:52 -0600

    Moved record types and record struct to interpreter.go

--- decoder.go
+++ decoder.go
@@ -5,26 +5,7 @@ import (
 	"errors"
 )
 
-//Intel hex record type
-type RecordType int
-
-const (
-	DataRecord	RecordType = iota // Data record type.
-	EndOfFileRecord // End of file record.
-	ExtendedSegmentAddressRecord // Extended segment address record.
-	ExtendedLinearAddressRecord // Extended linear address record
-	StartLinearAddressRecord // Start linear address record (MDK-ARM only).
-	ErrorRecord // Used if error in a record.
-)
-
-// Contains info about one record.
-type Record struct {
-	RecordType RecordType // The type of record.
-	Address uint32 // Address for bytes to be written to if data record.
-	Data []byte // Data bytes if a data record.
-}
-
-func DecodeString(IntelHexString string) (*Record, error) {
+func (i *Interpreter) DecodeString(IntelHexString string) (*Record, error) {
 	// Check for an empty string.
 	if IntelHexString == "" {
 		return &Record{
@@ -76,7 +57,7 @@ func DecodeString(IntelHexString string) (*Record, error) {
 	case DataRecord:
 		record.RecordType = DataRecord
 
-		recordLength := stringBytes[0]
+		//recordLength := stringBytes[0]
 		// Todo : Make sure correct number of bytes
 
 		// Add the data bytes to the records slice.
--- interpreter.go
+++ interpreter.go
@@ -1,5 +1,24 @@
 package intelhex
 
+//Intel hex record type
+type RecordType int
+
+const (
+	DataRecord	RecordType = iota // Data record type.
+	EndOfFileRecord // End of file record.
+	ExtendedSegmentAddressRecord // Extended segment address record.
+	ExtendedLinearAddressRecord // Extended linear address record
+	StartLinearAddressRecord // Start linear address record (MDK-ARM only).
+	ErrorRecord // Used if error in a record.
+)
+
+// Contains info about one record.
+type Record struct {
+	RecordType RecordType // The type of record.
+	Address uint32 // Address for bytes to be written to if data record.
+	Data []byte // Data bytes if a data record.
+}
+
 type Interpreter struct {
 
 }
